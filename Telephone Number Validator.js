d1efd98c7c18042e8914a9f83c9a167353467852
oluwafemiadeniyi <adeniyioluwafemi595@gmail.com>
2023-03-12 21:47:15 +0100

    Update Telephone Number Validator.js

--- Telephone Number Validator.js
+++ Telephone Number Validator.js
@@ -9,3 +9,4 @@ if(str[0]==1 && /[\s(]/.test(str[1]))
   return (/^1?\s?\d{3}[-\s]?\d{3}[-\s]?\d{4}$|^1?\s?[(]\d{3}[)]\s?\d{3}[-\s]?\d{4}$/.test(str) && 
   str.split(/[^0-9]|/).filter(a=>/\d/.test(a)).length==10);
 }
+console.log(telephoneCheck("555-555-5555"));
