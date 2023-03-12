07e4caea4efe994527959d6f5dd3acd24a87c8fe
oluwafemiadeniyi <adeniyioluwafemi595@gmail.com>
2023-03-12 21:46:24 +0100

    sencond instance of regex

--- Telephone Number Validator.js
+++ Telephone Number Validator.js
@@ -4,4 +4,8 @@ if(str[0]==1 && /[\s(]/.test(str[1]))
 //console.log(str.split(/[^0-9]|/)
 //create test and return the truth value of a regex for the first instance with country code
   return (/^1?\s?\d{3}[-\s]?\d{3}[-\s]?\d{4}$|^1?\s?[(]\d{3}[)]\s?\d{3}[-\s]?\d{4}$/.test(str) && 
-  str.split(/[^0-9]|/).filter(a=>/\d/.test(a)).length==11);}
+  str.split(/[^0-9]|/).filter(a=>/\d/.test(a)).length==11);
+  //test sencond instance of regex without country code
+  return (/^1?\s?\d{3}[-\s]?\d{3}[-\s]?\d{4}$|^1?\s?[(]\d{3}[)]\s?\d{3}[-\s]?\d{4}$/.test(str) && 
+  str.split(/[^0-9]|/).filter(a=>/\d/.test(a)).length==10);
+}
