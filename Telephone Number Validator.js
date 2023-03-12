d3e03071c25f3d96f4be59f6a6633f1b82bad433
oluwafemiadeniyi <adeniyioluwafemi595@gmail.com>
2023-03-12 21:44:42 +0100

    regex first instance test

--- Telephone Number Validator.js
+++ Telephone Number Validator.js
@@ -1,2 +1,7 @@
 //created telephone check function
-function telephoneCheck(str) {}
+function telephoneCheck(str) {
+if(str[0]==1 && /[\s(]/.test(str[1]))
+//console.log(str.split(/[^0-9]|/)
+//create test and return the truth value of a regex for the first instance with country code
+  return (/^1?\s?\d{3}[-\s]?\d{3}[-\s]?\d{4}$|^1?\s?[(]\d{3}[)]\s?\d{3}[-\s]?\d{4}$/.test(str) && 
+  str.split(/[^0-9]|/).filter(a=>/\d/.test(a)).length==11);}
